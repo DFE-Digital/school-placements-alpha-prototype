df7176d6d0ea8f337d5d724c57a8f251eafff7dc
Simon Whatley <simonwhatley@gmail.com>
2023-07-14 14:38:50 +0100

    Add placement model

---
+++ app/models/placements.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const fs = require('fs')
+const { v4: uuid } = require('uuid')
+
+
+exports.findMany = (params) => {
+
+}
+
+exports.findOne = (params) => {
+
+}
+
+exports.insertOne = (params) => {
+
+}
+
+exports.updateOne = (params) => {
+
+}
+
+exports.deleteOne = (params) => {
+
+}
