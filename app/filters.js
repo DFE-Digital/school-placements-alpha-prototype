02266e186824c4fa7d4f108c62773bd2a901dba1
Simon Whatley <simonwhatley@gmail.com>
2023-07-14 11:32:22 +0100

    Add getErrorMessage filter

--- app/filters.js
+++ app/filters.js
@@ -1,2 +1,19 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
+
+/* ------------------------------------------------------------------
+utility function to get an error for a component
+example: {{ errors | getErrorMessage('title') }}
+outputs: "Enter a title"
+------------------------------------------------------------------ */
+addFilter('getErrorMessage', (array, fieldName) => {
+  if (!array || !fieldName) {
+    return null
+  }
+
+  const error = array.filter((obj) =>
+    obj.fieldName === fieldName
+  )[0]
+
+  return error
+})
