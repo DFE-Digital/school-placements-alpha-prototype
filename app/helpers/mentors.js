3ba4c746b7c2f3d40146391093f9e97cfd0baa84
Simon Whatley <simonwhatley@gmail.com>
2023-07-20 16:15:34 +0100

    Add mentor helper

---
+++ app/helpers/mentors.js
@@ -0,0 +1,36 @@
+exports.getMentorOptions = (school, subject, selectedItem) => {
+  const items = []
+  const mentors = require('../data/temp/mentors')
+
+  mentors
+    .filter(mentor =>
+      mentor.school.id === school
+      && mentor.subject === subject
+    )
+    .forEach((mentor, i) => {
+      const item = {}
+
+      item.text = `${mentor.firstName} ${mentor.lastName}`
+      item.value = mentor.id
+      item.id = mentor.id
+      item.checked = (selectedItem && selectedItem.includes(mentor.id)) ? 'checked' : ''
+
+      items.push(item)
+    })
+
+  return items
+}
+
+exports.getMentorLabel = (code) => {
+  const mentors = require('../data/temp/mentors')
+  const mentor = mentors.find(mentor => mentor.id === code)
+console.log(code);
+console.log(mentor);
+  let label = code
+
+  if (mentor) {
+    label = `${mentor.firstName} ${mentor.lastName}`
+  }
+
+  return label
+}
