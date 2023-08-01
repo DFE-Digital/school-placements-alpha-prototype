4ec029dbf16b22d440b000c8949a6dce9bcbe81f
helengaskelldfe <139474990+helengaskelldfe@users.noreply.github.com>
2023-08-01 11:20:09 +0100

    typo

--- app/controllers/problemreport.js
+++ app/controllers/problemreport.js
@@ -1,5 +1,5 @@
 // When a GET request is made to the '/problemreport' route,
-// render the Nunjucks template found at './view/problemreport/index.njk'.
+// render the Nunchucks template found at './view/problemreport/index.njk'.
 exports.problemreport_get = (req, res) => {
     res.render('problemreport/index');
 }  
