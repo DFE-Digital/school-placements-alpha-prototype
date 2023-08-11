fec48eb08052fea417f611260e78354f1ad37515
Simon Whatley <simonwhatley@gmail.com>
2023-08-11 13:10:37 +0100

    Add auth routes

--- app/routes.js
+++ app/routes.js
@@ -11,6 +11,7 @@ const flash = require('connect-flash')
 router.use(flash())
 
 // Controller modules
+const authenticationController = require('./controllers/authentication')
 const organisationController = require('./controllers/organisations')
 const placementController = require('./controllers/placements')
 const resultsController = require('./controllers/results')
@@ -45,6 +46,46 @@ router.all('*', (req, res, next) => {
   res.locals.flash = req.flash('success') // pass through 'success' messages only
   next()
 })
+
+/// ------------------------------------------------------------------------ ///
+/// AUTHENTICATION ROUTES
+/// ------------------------------------------------------------------------ ///
+
+router.get('/sign-in', authenticationController.sign_in_get)
+router.post('/sign-in', passport.authenticate('local', {
+  successRedirect: '/auth',
+  failureRedirect: '/sign-in',
+  failureFlash: 'Enter valid sign-in details'
+}))
+
+router.get('/auth', authenticationController.auth_get)
+
+router.get('/sign-out', authenticationController.sign_out_get)
+
+router.get('/register', authenticationController.register_get)
+router.post('/register', authenticationController.register_post)
+
+router.get('/confirm-email', authenticationController.confirm_email_get)
+router.post('/confirm-email', authenticationController.confirm_email_post)
+
+router.get('/resend-code', authenticationController.resend_code_get)
+router.post('/resend-code', authenticationController.resend_code_post)
+
+router.get('/forgotten-password', authenticationController.forgotten_password_get)
+router.post('/forgotten-password', authenticationController.forgotten_password_post)
+
+router.get('/verification-code', authenticationController.verification_code_get)
+router.post('/verification-code', authenticationController.verification_code_post)
+
+router.get('/create-password', authenticationController.create_password_get)
+router.post('/create-password', authenticationController.create_password_post)
+
+router.get('/password-reset', authenticationController.password_reset_get)
+router.post('/password-reset', authenticationController.password_reset_post)
+
+router.get('/registration-complete', authenticationController.registration_complete_get)
+
+router.get('/terms-and-conditions', authenticationController.terms_and_conditions_get)
 
 /// ------------------------------------------------------------------------ ///
 /// PLACEMENT ROUTES
