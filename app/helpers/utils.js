855d10d0acd668167223ead363888838d78a959b
Simon Whatley <simonwhatley@gmail.com>
2023-08-14 18:14:33 +0100

    Add mentor utility functions

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -43,24 +43,6 @@ exports.removeFilter = (value, data) => {
   }
 }
 
-exports.getFilterALabel = (code) => {
-  let label = code
-
-  return label
-}
-
-exports.getFilterBLabel = (code) => {
-  let label = code
-
-  return label
-}
-
-exports.getFilterCLabel = (code) => {
-  let label = code
-
-  return label
-}
-
 exports.getAgeRangeFilterItems = (subjectLevel = 'secondary', selectedItems) => {
   const items = []
 
@@ -192,4 +174,171 @@ exports.getKeyStageFilterItems = (subjectLevel = 'secondary', selectedItems) => 
   })
 
   return items
+}
+
+
+// DUMMY DATA
+
+exports.getFilterAItems = (selectedItems) => {
+  const options = [
+    {
+      id: '0ec5b356-55b6-4f49-bc14-c978ebbe3790',
+      name: 'Option 1',
+      code: 'option-1'
+    },
+    {
+      id: '17afadc9-97b2-46fc-9737-e24063da2d1f',
+      name: 'Option 2',
+      code: 'option-2'
+    },
+    {
+      id: '6a1c523a-1bdd-49a2-a691-b152cf838303',
+      name: 'Option 3',
+      code: 'option-3'
+    }
+  ]
+
+  const items = []
+
+  options.forEach((option, i) => {
+    const item = {}
+
+    item.text = option.name
+    item.value = option.code
+    item.id = option.id
+    item.checked = (selectedItems && selectedItems.includes(option.code)) ? 'checked' : ''
+
+    items.push(item)
+  })
+
+  return items
+}
+
+exports.getFilterBItems = (selectedItems) => {
+  const options = [
+    {
+      id: 'eef4b893-d53d-4df6-8fe5-a75418c3d25a',
+      name: 'Option 1',
+      code: 'option-1'
+    },
+    {
+      id: '18c4c365-daa0-405b-ab9c-680d3b9a85f6',
+      name: 'Option 2',
+      code: 'option-2'
+    },
+    {
+      id: 'bfdc3f06-0f2f-4ee7-9f6f-9d9f0ee19c83',
+      name: 'Option 3',
+      code: 'option-3'
+    }
+  ]
+
+  const items = []
+
+  options.forEach((option, i) => {
+    const item = {}
+
+    item.text = option.name
+    item.value = option.code
+    item.id = option.id
+    item.checked = (selectedItems && selectedItems.includes(option.code)) ? 'checked' : ''
+
+    items.push(item)
+  })
+
+  return items
+}
+
+exports.getFilterCItems = (selectedItems) => {
+  const options = [
+    {
+      id: '373ba7e6-f2a3-4de8-908c-a630e6e00bdc',
+      name: 'Option 1',
+      code: 'option-1'
+    },
+    {
+      id: '7bd46af4-0dc2-4376-a852-a055ce15c3af',
+      name: 'Option 2',
+      code: 'option-2'
+    },
+    {
+      id: '060a7b6b-cbbb-48ac-a78c-03e24b783dda',
+      name: 'Option 3',
+      code: 'option-3'
+    }
+  ]
+
+  const items = []
+
+  options.forEach((option, i) => {
+    const item = {}
+
+    item.text = option.name
+    item.value = option.code
+    item.id = option.id
+    item.checked = (selectedItems && selectedItems.includes(option.code)) ? 'checked' : ''
+
+    items.push(item)
+  })
+
+  return items
+}
+
+exports.getFilterDItems = (selectedItems) => {
+  const options = [
+    {
+      id: 'b5995d91-a26e-4c4d-b338-30c1d187ece8',
+      name: 'Option 1',
+      code: 'option-1'
+    },
+    {
+      id: 'a2fce60f-d211-44cc-94fc-50c9c4145d67',
+      name: 'Option 2',
+      code: 'option-2'
+    },
+    {
+      id: 'aed87067-6aad-4898-8cb0-377d4f84ab99',
+      name: 'Option 3',
+      code: 'option-3'
+    }
+  ]
+
+  const items = []
+
+  options.forEach((option, i) => {
+    const item = {}
+
+    item.text = option.name
+    item.value = option.code
+    item.id = option.id
+    item.checked = (selectedItems && selectedItems.includes(option.code)) ? 'checked' : ''
+
+    items.push(item)
+  })
+
+  return items
+}
+
+exports.getFilterALabel = (code) => {
+  let label = code
+
+  return label
+}
+
+exports.getFilterBLabel = (code) => {
+  let label = code
+
+  return label
+}
+
+exports.getFilterCLabel = (code) => {
+  let label = code
+
+  return label
+}
+
+exports.getFilterDLabel = (code) => {
+  let label = code
+
+  return label
 }
